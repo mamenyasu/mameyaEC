1b85cee18fa093e3273cab4ffbf57af52c590c19
mamenyasu <laudo_shell@ybb.ne.jp>
2026-06-17 09:08:52 +0900

    不要なファイルを削除

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\MaintenanceMode;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\MainEC;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', [MemberIndexController::class, 'mainEC'])->name('welcome');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-    Route::get('memberIndex', [MemberIndexController::class, 'membersIndex'])->name('memberIndex')->middleware('admin');
-    Route::get('mainEC', [MemberIndexController::class, 'mainEC'])->name('mainEC');
-    Route::post('/cart/add', [MemberIndexController::class, 'add']);
-    Route::get('/cart', [MemberIndexController::class, 'cartIndex'])->name('cartIndex');
-    Route::post('/cart/remove', [MemberIndexController::class, 'cartRemove'])->name('cartRemove');
-    Route::get('/cart/checkout', [MemberIndexController::class, 'checkoutIndex'])->name('checkout');
-    Route::post('/checkout/process', [MemberIndexController::class, 'checkoutProcess'])->name('checkout.process');
-    Route::get('/checkout/complete', function () {
-        return view('checkout.complete');
-    })->name('checkout.complete');
-});
+Route::view('dashboard', 'dashboard')->name('dashboard');
+Route::get('memberIndex', [MemberIndexController::class, 'membersIndex'])->name('memberIndex')->middleware('admin');
+Route::get('mainEC', [MemberIndexController::class, 'mainEC'])->name('mainEC');
+Route::post('/cart/add', [MemberIndexController::class, 'add']);
+Route::get('/cart', [MemberIndexController::class, 'cartIndex'])->name('cartIndex');
+Route::post('/cart/remove', [MemberIndexController::class, 'cartRemove'])->name('cartRemove');
+Route::get('/cart/checkout', [MemberIndexController::class, 'checkoutIndex'])->name('checkout');
+Route::post('/checkout/process', [MemberIndexController::class, 'checkoutProcess'])->name('checkout.process');
+Route::get('/checkout/complete', function () {
+    return view('checkout.complete');
+})->name('checkout.complete');
 
 require __DIR__ . '/settings.php';
